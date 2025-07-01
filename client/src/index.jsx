c2e660e97bd855c28ae1c87988553a25d6c10367
mubu002 <mustafa_buga1@hotmail.com>
2025-07-01 21:38:36 +0200

    I did a lot of shit i real try to understand, but hopefully it will get better.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import * as ReactDom from "react-dom";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+import {isCorrectAnswer} from "./questions";
+import {randomQuestion} from "./questions";
+
+function QuizApplication(){
+
+    const [question, setQuestion] = useState(randomQuestion);
+    const [answer, setAnswer] = useState();
+
+    if (answer) {
+        return <>
+            <h1>{isCorrectAnswer(question, answer)? "Right" : "Wrong"}</h1>;
+            <p>
+                <button onClick={() => {
+                    setQuestion(randomQuestion());
+                    setAnswer(undefined);
+                }}>Another question</button>
+            </p>
+        </>;
+    }
+
+    return <>
+        <h1>question.question</h1>
+        {Object.keys(question.answers).filter(a => question.answers[a]).map(a => <p key={a}>
+            <button onClick={() => setAnswer(a)}>{question.answers[a]}</button></p>)}
+        </>;
+}
+
+
+ReactDom.render(<h1>Take a Quiz</h1>, document.getElementById("root"));
 
